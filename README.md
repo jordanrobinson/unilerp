fa59228715fb0429d5de151645294895ed75ce33
Jordan Robinson <hypahnova@gmail.com>
2016-04-28 21:27:09 +0100

    Adds initial implementation and readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# unilerp
-UniLerp is a small unity library for smoothly transitioning game objects between position, scale and rotation
+# UniLerp
+UniLerp is a small Unity library for smoothly transitioning game objects between position, scale and rotation
+
+## What is this?
+
+This is a (very) small library for LERPing game objects in unity between positions, scale and rotation.
+
+## How do I use it?
+
+Simply include somewhere in your C# project, then use the StartLerp method to register a game object for a transition. Then, use the Tick method somewhere on an update method on a Unity Behaviour.
+
+This should looks somewhat like this:
+
+	using UnityEngine;
+
+	public class FooBehaviour : MonoBehaviour
+	{
+	    public GameObject Goal;
+
+	    void Start()
+	    {
+	        UniLerp.StartLerp(Goal, new Vector3(-30.83f, 1.0f, 1.0f), new Vector3(100f, 90f, 90f), new Vector3(1.0f, 1.0f, 1.0f));
+	    }
+
+	    void Update()
+	    {
+	        UniLerp.Tick();
+		}
+	}
+
+With Goal being passed in through the Unity editor as a parameter.
+
+## When will it update?
+
+If I need further features or capabilities, I'll be adding them. If you have anything extra you need from the library, or any improvements you can offer, please make a pull request!
+
+## Who did this?
+[That would be me.](http://jordanrobinson.co.uk)
---
+++ UniLerp.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class UniLerp
+{
+    private static readonly List<Lerpable> Lerpables = new List<Lerpable>();
+
+    /// <summary>
+    /// Checks all lerpable items set up and steps through each, applying the movement required.
+    /// </summary>
+    public static void Tick()
+    {
+
+        if (IsLerping())
+        {
+            foreach (var lerpable in Lerpables.Where(lerpable => lerpable.Lerping))
+            {
+                LerpStep(lerpable);
+            }
+        }
+    }
+
+    private static bool IsLerping()
+    {
+        return Lerpables.Any(lerpable => lerpable.Lerping);
+    }
+
+    /// <summary>
+    /// Starts a lerp transition for a given game object
+    /// </summary>
+    /// <param name="gameObject">The game object to move</param>
+    /// <param name="targetPosition">The target position of the game object, can be omitted if no transition required</param>
+    /// <param name="targetRotation">The target rotation of the game object, can be omitted if no transition required</param>
+    /// <param name="targetScale">The target scale of the game object, can be omitted if no transition required</param>
+    public static void StartLerp(GameObject gameObject, Vector3 targetPosition = new Vector3(), Vector3 targetRotation = new Vector3(), Vector3 targetScale = new Vector3())
+    {
+        var lerpable = new Lerpable(gameObject);
+
+        lerpable.StartPosition = lerpable.InnerGameObject.transform.position;
+        lerpable.TargetPosition = targetPosition;
+
+        lerpable.StartAngle = lerpable.InnerGameObject.transform.eulerAngles;
+        lerpable.TargetAngle = targetRotation;
+
+        lerpable.StartScale = lerpable.InnerGameObject.transform.localScale;
+        lerpable.TargetScale = targetScale;
+
+        lerpable.StartTime = Time.time;
+
+        lerpable.JourneyLength = Vector3.Distance(lerpable.StartPosition, lerpable.TargetPosition);
+        lerpable.AngleLength = Vector3.Distance(lerpable.StartAngle, lerpable.TargetAngle);
+        lerpable.ScaleLength = Vector3.Distance(lerpable.StartScale, lerpable.TargetScale);
+
+        lerpable.Lerping = true;
+
+        Lerpables.Add(lerpable);
+    }
+
+    private static void LerpStep(Lerpable lerpable)
+    {
+        // position
+        var distCovered = (Time.time - lerpable.StartTime) * lerpable.PositionSpeed;
+        var fracJourney = distCovered / lerpable.JourneyLength;
+        lerpable.InnerGameObject.transform.position = Vector3.Lerp(lerpable.StartPosition, lerpable.TargetPosition, fracJourney);
+        // angle
+        var angleCovered = (Time.time - lerpable.StartTime) * lerpable.AngleSpeed;
+        var angleJourney = angleCovered / lerpable.AngleLength;
+        lerpable.InnerGameObject.transform.eulerAngles = Vector3.Lerp(lerpable.StartAngle, lerpable.TargetAngle, angleJourney);
+        // scale
+        var scaleCovered = (Time.time - lerpable.StartTime) * lerpable.ScaleSpeed;
+        var scaleJourney = scaleCovered / lerpable.ScaleLength;
+        lerpable.InnerGameObject.transform.localScale = Vector3.Lerp(lerpable.StartScale, lerpable.TargetScale, scaleJourney);
+
+        if (angleJourney > 1.0f
+            && lerpable.InnerGameObject.transform.position == lerpable.TargetPosition
+            && lerpable.InnerGameObject.transform.localScale == lerpable.TargetScale)
+        {
+            lerpable.Lerping = false;
+        }
+    }
+}
+
+public class Lerpable
+{
+    public Lerpable(GameObject gameObject, float positionSpeed = 10.0f, float angleSpeed = 60.0f, float scaleSpeed = 100.0f)
+    {
+        InnerGameObject = gameObject;
+
+        PositionSpeed = positionSpeed;
+        AngleSpeed = angleSpeed;
+        ScaleSpeed = scaleSpeed;
+    }
+
+    public float StartTime { get; set; }
+
+    public float JourneyLength { get; set; }
+    public float AngleLength { get; set; }
+    public float ScaleLength { get; set; }
+
+    public readonly float PositionSpeed;
+    public readonly float AngleSpeed;
+    public readonly float ScaleSpeed;
+    public bool Lerping { get; set; }
+
+    public Vector3 StartPosition { get; set; }
+    public Vector3 TargetPosition { get; set; }
+
+    public Vector3 StartAngle { get; set; }
+    public Vector3 TargetAngle { get; set; }
+
+    public Vector3 StartScale { get; set; }
+    public Vector3 TargetScale { get; set; }
+
+    public readonly GameObject InnerGameObject;
+}
